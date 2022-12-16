7bd0eb18064b537e16036661c5c23d34f03c4431
Theta42 <26573482+RachellLiu@users.noreply.github.com>
2022-12-16 00:05:58 +0000

    Update Day9_String2.cpp

--- Day9_String2.cpp
+++ Day9_String2.cpp
@@ -1,5 +1,47 @@
 /*****************************************************************
 
+  28. Find the Index of the First Occurrence in a String
+  
+  KMP algorithm:
+  如果用暴力解法时间复杂度是O(文本串长度*模式串长度)，而KMP会小很多
+  
 
 ******************************************************************/
 
+class Solution {
+public:
+    void getNext (int* next, const string& s) {
+        int j = 0;
+        next[0] = 0;
+        for (int i = 1; i < s.size(); i++) {
+            while (j > 0 && s[i] != s[j]) {
+                j = next[j-1];
+            }
+            if (s[i] == s[j]) {
+                j++;
+            }
+            next[i] = j;
+        }
+    }
+
+    int strStr(string haystack, string needle) {
+        if (needle.size() == 0) {
+            return 0;
+        }
+        int next[needle.size()];
+        getNext(next,needle);
+        int j = 0;
+        for (int i = 0; i < haystack.size(); i++) {
+            while (j > 0 && haystack[i] != needle[j]) {
+                j = next[j-1];
+            }
+            if (haystack[i] == needle[j]) {
+                j++;
+            }
+            if (j == needle.size()) {
+                return (i - needle.size() + 1);
+            }
+        }
+        return -1;
+    }
+};
