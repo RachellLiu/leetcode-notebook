e7a671a05768a91a3527f3f024397c6de3b871d4
Theta42 <26573482+RachellLiu@users.noreply.github.com>
2022-12-17 22:56:14 +0000

    Update Day11_Stack_Queue2.cpp

--- Day11_Stack_Queue2.cpp
+++ Day11_Stack_Queue2.cpp
@@ -22,3 +22,50 @@ public:
     }
 };
 
+/**************************************************************
+
+  1047. Remove All Adjacent Duplicates In String
+  
+  先用stack写的，然后发现用string更简单
+
+**************************************************************/
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string results;
+        for (char a: s) {
+            if (results.empty() || a != results.back()) results.push_back(a);
+            else results.pop_back();
+        }
+        return results;
+    }
+};
+
+/**************************************************************
+
+  150. Evaluate Reverse Polish Notation
+
+**************************************************************/
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<long> st;
+        for (int i = 0; i < tokens.size(); i++) {
+            if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/") {
+                long t2 = st.top();
+                st.pop();
+                long t1 = st.top();
+                st.pop();
+                if (tokens[i] == "+") st.push(t1+t2);
+                if (tokens[i] == "-") st.push(t1-t2);
+                if (tokens[i] == "*") st.push(t1*t2);
+                if (tokens[i] == "/") st.push(t1/t2);
+            }else{
+                st.push(stol(tokens[i]));  // Convert string to long int: s to l
+            }
+        }
+        return st.top();
+    }
+};
