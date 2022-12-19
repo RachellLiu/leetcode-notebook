09d05ba338b83eb17ce6ebc55655ce9900d14fa4
Theta42 <26573482+RachellLiu@users.noreply.github.com>
2022-12-19 21:42:37 +0000

    Update Day13_Stack_Queue3.cpp

--- Day13_Stack_Queue3.cpp
+++ Day13_Stack_Queue3.cpp
@@ -2,5 +2,51 @@
 
   239. Sliding Window Maximum
   
+  写一个单调队列的class
+  
+*************************************************************************/
+
+class Solution {
+private:
+    class myDeque {
+        public:
+            deque<int> que;
+            void pop(int value) {
+                if (!que.empty() && value == que.front()) {
+                    que.pop_front();
+                }
+            }
+            void push(int value) {
+                while (!que.empty() && value > que.back()) {
+                    que.pop_back();
+                }
+                que.push_back(value);
+            }
+            int front () {
+                return que.front();
+            }
+    };
+
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        myDeque que;
+        vector<int> result;
+        for (int i = 0; i < k; i++) {
+            que.push(nums[i]);
+        }
+        result.push_back(que.front());
+        for (int i = k; i < nums.size(); i++) {
+            que.pop(nums[i - k]);
+            que.push(nums[i]);
+            result.push_back(que.front());
+        }
+        return result;
+    }
+};
+
+/*************************************************************************
+
+  347. Top K Frequent Elements
+  
 *************************************************************************/
 
