742affbd51e6abd1fbc3d762fad918eb709a3945
Theta42 <26573482+RachellLiu@users.noreply.github.com>
2022-12-19 22:15:07 +0000

    Update Day13_Stack_Queue3.cpp

--- Day13_Stack_Queue3.cpp
+++ Day13_Stack_Queue3.cpp
@@ -48,5 +48,36 @@ public:
 
   347. Top K Frequent Elements
   
+  用小顶堆
+  
 *************************************************************************/
 
+class Solution {
+public:
+    class myComparison {
+        public:
+            bool operator()(const pair<int,int>& lhs, const pair<int,int>& rhs){
+                return lhs.second > rhs.second;
+            }
+    };
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            map[nums[i]] ++;
+        }
+        priority_queue<pair<int,int>,vector<pair<int,int>>, myComparison> pri_que;
+        for(unordered_map<int,int>::iterator it=map.begin();it!=map.end();it++){
+            pri_que.push(*it);
+            if (pri_que.size()>k) {
+                pri_que.pop();
+            }
+        }
+        vector<int> result(k);
+        for (int i = k - 1; i >= 0; i--) {
+            result[i] = pri_que.top().first;
+            pri_que.pop();
+        }
+        return result;
+    }
+};
