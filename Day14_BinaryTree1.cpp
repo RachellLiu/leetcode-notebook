6542b0350c2b92714cf4a26a12f6e7ada6a52353
Theta42 <26573482+RachellLiu@users.noreply.github.com>
2022-12-21 19:01:20 +0000

    Update Day14_BinaryTree1.cpp

--- Day14_BinaryTree1.cpp
+++ Day14_BinaryTree1.cpp
@@ -8,6 +8,7 @@
   
 ********************************************************/
 
+//递归法
 class Solution {
 public:
     void traversal (TreeNode* cur, vector<int>& vec) {
@@ -23,12 +24,32 @@ public:
     }
 };
 
+//迭代法
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        vector<int> result;
+        if (root == NULL) return result;
+        st.push(root);
+        while (!st.empty()) {
+            TreeNode* node = st.top();                     
+            st.pop();
+            result.push_back(node->val);
+            if (node->right) st.push(node->right);          
+            if (node->left) st.push(node->left);            
+        }
+        return result;
+    }
+};
+
 /********************************************************
 
   145. Binary Tree Postorder Traversal 后序遍历（左右中）
   
 ********************************************************/
 
+//递归法
 class Solution {
 public:
     void traversal (TreeNode* cur, vector<int>& vec) {
@@ -44,12 +65,33 @@ public:
     }
 };
 
+//迭代法
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        vector<int> result;
+        if (root == NULL) return result;
+        st.push(root);
+        while (!st.empty()) {
+            TreeNode* node = st.top();
+            st.pop();
+            result.push_back(node->val);
+            if (node->left) st.push(node->left);
+            if (node->right) st.push(node->right);
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
+
 /********************************************************
 
-  94. Binary Tree Inorder Traversal 中序遍历 (左中右)
+  94. Binary Tree Inorder Traversal 中序遍历 (左中interior nodes右)
   
 ********************************************************/
 
+//递归法
 class Solution {
 public:
     void traversal (TreeNode* cur, vector<int>& vec) {
@@ -61,6 +103,29 @@ public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> result;
         traversal(root, result);
+        return result;
+    }
+};
+
+//迭代法
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        vector<int> result;
+        if (root == NULL) return result;
+        TreeNode* cur = root;
+        while (cur != NULL || !st.empty()) {
+            if (cur != NULL) {
+                st.push(cur);
+                cur = cur->left;
+            }else{
+                cur = st.top();
+                st.pop();
+                result.push_back(cur->val);
+                cur = cur->right;
+            }
+        }
         return result;
     }
 };
