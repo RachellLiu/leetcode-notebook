cc3a65155c35e054449dde1b92343c097b7012ee
Theta42 <26573482+RachellLiu@users.noreply.github.com>
2022-12-23 17:16:18 +0000

    Update Day15_BinaryTree2.cpp

--- Day15_BinaryTree2.cpp
+++ Day15_BinaryTree2.cpp
@@ -188,3 +188,161 @@ public:
 
 ***************************************************************************/
 
+class Solution {
+public:
+    Node* connect(Node* root) {
+        queue<Node*> que;
+        if (root != NULL) que.push(root);
+        while (!que.empty()) {
+            int size = que.size();
+            while (size--) {
+                Node* node = que.front();
+                que.pop();
+                if (size != 0){
+                    node->next = que.front();
+                }else{
+                    node->next = NULL;
+                }
+                if (node->left) que.push(node->left);
+                if (node->right) que.push(node->right);
+            }
+            
+        }
+        return root;
+    }
+};
+
+/***************************************************************************
+
+  117. Populating Next Right Pointers in Each Node II
+  
+  层序遍历8
+  和上题答案一模一样解决了......
+
+***************************************************************************/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        queue<Node*> que;
+        if (root != NULL) que.push(root);
+        while (!que.empty()) {
+            int size = que.size();
+            while (size--) {
+                Node* node = que.front();
+                que.pop();
+                if (size != 0){
+                    node->next = que.front();
+                }else{
+                    node->next = NULL;
+                }
+                if (node->left) que.push(node->left);
+                if (node->right) que.push(node->right);
+            }
+            
+        }
+        return root;
+    }
+};
+
+/***************************************************************************
+
+  104. Maximum Depth of Binary Tree
+  
+  层序遍历9
+
+***************************************************************************/
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        queue<TreeNode*> que;
+        int depth = 0;
+        if (root != NULL) que.push(root);
+        while (!que.empty()) {
+            int size = que.size();
+            while (size--) {
+                TreeNode* node = que.front();
+                que.pop();
+                if (node->left) que.push(node->left);
+                if (node->right) que.push(node->right);
+            }
+            depth++;
+        }
+        return depth;
+    }
+};
+
+/***************************************************************************
+
+  111. Minimum Depth of Binary Tree
+  
+  层序遍历10
+
+***************************************************************************/
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        queue<TreeNode*> que;
+        int depth = 0;
+        if (root != NULL) que.push(root);
+        while (!que.empty()) {
+            int size = que.size();
+            depth++;
+            while (size--) {
+                TreeNode* node = que.front();
+                que.pop();
+                if (node->left) que.push(node->left);
+                if (node->right) que.push(node->right);
+                if (node->right == NULL && node->left == NULL) return depth;
+            }
+        }
+        return depth;
+    }
+};
+
+/***************************************************************************
+
+  226. Invert Binary Tree
+  这里用的前序遍历，前后都可以直接按顺序写，但是中序注意把右依然写成左
+
+***************************************************************************/
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == NULL) return root;
+        swap(root->left, root->right);
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
+
+/***************************************************************************
+
+  101. Symmetric Tree
+  这里用的前序遍历
+
+***************************************************************************/
+
+class Solution {
+public:
+    bool compare(TreeNode* left, TreeNode* right) {
+        if (left == NULL && right != NULL) return false;
+        else if (left != NULL && right == NULL) return false;
+        else if (left == NULL && right == NULL) return true;
+        else if (left->val != right->val) return false;
+
+        bool outside = compare(left->left, right->right);
+        bool inside = compare(left->right, right->left);
+        bool result = outside && inside;
+        return result;
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if (root == NULL) return true;
+        return compare(root->left, root->right);
+    }
+};
