ea47f7178e8ea406f3fec067f6544ee7d93bbd38
Theta42 <26573482+RachellLiu@users.noreply.github.com>
2022-12-24 16:46:35 +0000

    Update Day17_BinaryTree4.cpp

--- Day17_BinaryTree4.cpp
+++ Day17_BinaryTree4.cpp
@@ -4,3 +4,26 @@
 
 ****************************************************************/
 
+class Solution {
+public:
+    int getHeight(TreeNode* root) {
+        if (root == NULL) return 0;
+        int leftHeight = getHeight(root->left);
+        if (leftHeight == -1) return -1;
+        int rightHeight = getHeight(root->right);
+        if (rightHeight == -1) return -1;
+        if (abs(leftHeight - rightHeight) > 1) return -1;
+        else return 1 + max(leftHeight, rightHeight);
+    }
+    bool isBalanced(TreeNode* root) {
+        if (getHeight(root) == -1) return false;
+        else return true;
+    }
+};
+
+/****************************************************************
+
+  257. Binary Tree Paths
+
+****************************************************************/
+
