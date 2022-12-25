fef081168515569697b556e5ca72c3782ed062b4
Theta42 <26573482+RachellLiu@users.noreply.github.com>
2022-12-25 13:22:42 +0000

    Update Day18_BinaryTree5.cpp

--- Day18_BinaryTree5.cpp
+++ Day18_BinaryTree5.cpp
@@ -82,8 +82,36 @@ public:
     }
 };
 
-/******************************************************
+/******************************************************************************
 
-  112. Path Sum
+  106. Construct Binary Tree from Inorder and Postorder Traversal
 
-******************************************************/
+******************************************************************************/
+
+class Solution {
+public:
+    TreeNode* traversal(vector<int>& inorder, vector<int>& postorder) {
+        if (postorder.size() == 0) return NULL;
+        int rootValue = postorder[postorder.size()-1];
+        TreeNode* root = new TreeNode(rootValue);
+        if (postorder.size() == 1) return root;
+
+        int delimiterIndex;
+        for (delimiterIndex=0; delimiterIndex < inorder.size(); delimiterIndex++){
+            if (inorder[delimiterIndex] == rootValue) break;
+        }
+        vector<int> leftInorder(inorder.begin(), inorder.begin()+delimiterIndex);
+        vector<int> rightInorder(inorder.begin()+delimiterIndex+1,inorder.end());
+        postorder.resize(postorder.size()-1);
+        vector<int> leftPostorder(postorder.begin(),postorder.begin()+leftInorder.size());
+        vector<int> rightPostorder(postorder.begin()+leftInorder.size(), postorder.end());
+
+        root->left = traversal(leftInorder, leftPostorder);
+        root->right = traversal(rightInorder, rightPostorder);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        if (inorder.size() == 0 || postorder.size() == 0) return NULL;
+        return traversal(inorder,postorder);
+    }
+};
