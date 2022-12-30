3b896fdbd1a4d3f31514735d1c063c6bdc4730b2
Theta42 <26573482+RachellLiu@users.noreply.github.com>
2022-12-30 22:37:57 +0000

    Update Day21_BinaryTree7.cpp

--- Day21_BinaryTree7.cpp
+++ Day21_BinaryTree7.cpp
@@ -26,5 +26,55 @@ public:
 
   501. Find Mode in Binary Search Tree
   
+  二叉搜索树一定用中序遍历
+  
 ****************************************************/
 
+class Solution {
+public:
+    TreeNode* pre = NULL;
+    int count = 0, maxCount = 0;
+    vector<int> result;
+    void traversal(TreeNode* cur) {
+        if (cur == NULL) return;
+        traversal(cur->left);
+        if (pre == NULL) count = 1;
+        else if (pre->val == cur->val) count++;
+        else count = 1;
+        pre = cur;
+        if (count == maxCount) result.push_back(cur->val);
+        if (count > maxCount) {
+            maxCount = count;
+            result.clear();
+            result.push_back(cur->val);
+        }
+        traversal(cur->right);
+        return;
+    }
+    vector<int> findMode(TreeNode* root) {
+        //如果上面的pre，count，maxCount都是private的话，这里需要定义一下
+        traversal(root);
+        return result;
+    }
+};
+
+/****************************************************
+
+  236. Lowest Common Ancestor of a Binary Tree
+  
+  回溯
+  
+****************************************************/
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == NULL || root == p || root == q) return root;
+        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
+        if (left != NULL && right !=NULL) return root;
+        else if (left == NULL && right != NULL) return right;
+        else if (left != NULL && right == NULL) return left;
+        else return NULL;
+    }
+};
